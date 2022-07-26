8100ef8706b5fd841fce14902db02c35a69a8bd8
conanxiao <conanxiao@tencent.com>
2022-07-26 23:23:53 +0800

    feat: 测试证书

--- backend/main.go
+++ backend/main.go
@@ -10,7 +10,7 @@ import (
 func main() {
 	app := iris.New()
 	mvc.Configure(app.Party("/user"), userController)
-	app.Run(iris.TLS("127.0.0.1:443","a.crt","b.key"))
+	app.Run(iris.TLS("127.0.0.1:433","a.crt","b.key"))
 }
 
 func userController(app *mvc.Application) {
