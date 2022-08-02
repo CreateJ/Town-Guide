1ee02ee21e6472afa7bdbbfc7ee929866d9448da
wangchijie <905260054@qq.com>
2022-08-02 23:39:14 +0800

    feat: add wx map component

--- wx/dist/pages/map/index.js
+++ wx/dist/pages/map/index.js
@@ -9,16 +9,40 @@ Page({
   },
 
   // 事件处理函数
-  onLoad() {
-    console.log(this.data.msg);
-    wx.request({
-      url: 'https://guide.time-traveler.cn/user/1',
-      method: 'GET',
+  onLoad() {// wx.request({
+    //   url: 'https://guide.time-traveler.cn/user/1',
+    //   method: 'GET',
+    //   success(res) {
+    //     console.log(res)
+    //   }
+    // })
+  },
 
-      success(res) {
-        console.log(res);
+  onReady() {
+    this.mapCtx = wx.createMapContext('easyMap');
+    console.log(this.mapCtx);
+    this.mapCtx.addGroundOverlay({
+      id: 1,
+      src: 'https://guide.time-traveler.cn/utils/getPic/1659283570猫猫3.jpeg',
+      bounds: {
+        southwest: {
+          longitude: 116.802075,
+          latitude: 23.541833
+        },
+        northeast: {
+          longitude: 116.863016,
+          latitude: 23.587412
+        }
+      },
+      visible: true,
+      zIndex: 1000,
+      success: res => {
+        console.log(res, 1111);
+        console.log(res, 1111);
+      },
+      fail: err => {
+        console.log(err);
       }
-
     });
   }
 
