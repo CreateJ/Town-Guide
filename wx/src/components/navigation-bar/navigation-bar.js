7099d639abadc452886251facea929065c87a659
wangchijie <905260054@qq.com>
2022-08-04 00:03:57 +0800

    feat 完成顶部状态栏按钮切换

--- wx/src/components/navigation-bar/navigation-bar.js
+++ wx/src/components/navigation-bar/navigation-bar.js
@@ -7,7 +7,8 @@ Component({
       value: {
         title: "我是默认标题"
       },
-      observer: function(newVal, oldVal) {}
+      observer: function (newVal, oldVal) {
+      }
     }
   },
   data: {
@@ -15,7 +16,42 @@ Component({
     menuRight: app.globalData.menuRight,
     menuTop: app.globalData.menuTop,
     menuHeight: app.globalData.menuHeight,
+    active: null,
+    button1Bg: '../../assets/image/point.png',
+    button2Bg: '../../assets/image/list.png'
   },
-  attached: function() {},
-  methods: {}
+  attached: function () {
+  },
+  methods: {
+    clickCategory() {
+      if (this.data.active === 0) {
+        this.data.active = null
+        this.setData({
+          button1Bg: '../../assets/image/point.png'
+        })
+      } else {
+        this.data.active = 0
+        this.setData({
+          button1Bg: '../../assets/image/point-active.png',
+          button2Bg: '../../assets/image/list.png',
+        })
+      }
+    },
+    clickList() {
+      if (this.data.active === 1) {
+        this.data.active = null
+        this.setData({
+          button2Bg: '../../assets/image/list.png',
+          button1Bg: '../../assets/image/point.png'
+        })
+      } else {
+        this.data.active = 1
+        this.setData({
+          button2Bg: '../../assets/image/list-active.png',
+          button1Bg: '../../assets/image/point.png',
+        })
+      }
+    }
+  },
+  lifetimes: {},
 })
